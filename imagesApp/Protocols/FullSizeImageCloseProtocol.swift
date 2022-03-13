e34d182e2f2948f538e8840fe14b1a58603180d3
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-13 22:21:28 +0600

    Create FullSizeImageCloseProtocol.swift

---
+++ imagesApp/Protocols/FullSizeImageCloseProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  FullSizeImageCloseProtocol.swift
+//  ImagesApp
+//
+//  Created by Ongar.dev on 13/03/2022.
+//  Copyright © 2022 Ongar.dev All rights reserved.
+//
+
+protocol FullSizeImageCloseProtocol {
+    func closed()
+}
