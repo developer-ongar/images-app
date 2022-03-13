9cd93724e937a7ec11f4e713d7dbfeeaaa3f518d
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-13 22:24:22 +0600

    Create NotificationNameExtension.swift

---
+++ imagesApp/Extensions/NotificationNameExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNameExtension.swift
+//  ImagesApp
+//
+//  Created by Ongar.dev on 13/03/2022.
+//  Copyright © 2022 Ongar.dev All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let getDataFinished = Notification.Name("getDataFinished")
+}
